8080a73823cd1d36ec490a211e59c4c613c7e7ae
Allana Anderson <allana.anderson17@gmail.com>
2020-10-13 14:22:10 -0600

    styled the meet eric section

--- src/Components/MeetEric/MeetEric.js
+++ src/Components/MeetEric/MeetEric.js
@@ -6,14 +6,14 @@ function MeetEric() {
   return (
     <>
         <div className="container" id="meet-eric-container">
-          <div className="meet-header">
-            <h2>Meet Eric!</h2>
-          </div>
-          <div class="meet-eric-wrapper">
-            <div class="meet-eric-right">
+          <div className="meet-eric-wrapper">
+            <div className="meet-eric-right">
+            <h1>Meet Eric Anderson</h1>
+            <p> Sed euismod nisi est, id fermentum ex semper in. Aenean tincidunt enim non nunc molestie laoreet. Sed pretium neque lacinia augue vulputate, ac fringilla tortor gravida. Pellentesque quis hendrerit lorem. Pellentesque accumsan interdum lacus. Nunc non justo a nisi condimentum porta nec id nibh. Sed sed iaculis ante. Curabitur viverra dolor sed rhoncus mollis. </p>
+            <br/>
             <p> Sed euismod nisi est, id fermentum ex semper in. Aenean tincidunt enim non nunc molestie laoreet. Sed pretium neque lacinia augue vulputate, ac fringilla tortor gravida. Pellentesque quis hendrerit lorem. Pellentesque accumsan interdum lacus. Nunc non justo a nisi condimentum porta nec id nibh. Sed sed iaculis ante. Curabitur viverra dolor sed rhoncus mollis. </p>
             </div>
-            <div class="meet-eric-left"></div>
+            <div className="meet-eric-left"></div>
           </div>
         </div>
     </>

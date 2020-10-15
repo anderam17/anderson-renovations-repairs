3386f629570b444254870ddffecf68af8659ab26
Allana Anderson <allana.anderson17@gmail.com>
2020-10-15 10:56:50 -0600

    created container for review cards that iterates through all of the reviews in the json file.

---
+++ src/Components/ReviewContainer/ReviewContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ReviewContainer.css";
+import ReviewCard from "../ReviewCard/ReviewCard";
+import reviews from "../../reviews.json";
+
+function ReviewContainer() {
+  return (
+    <>
+      <div id="review-cards">
+        {reviews.map((review) => (
+          <ReviewCard
+            name={review.name}
+            location={review.location}
+            job_description={review.job_description}
+            work_quality={review.work_quality}
+            eric={review.eric}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default ReviewContainer;
